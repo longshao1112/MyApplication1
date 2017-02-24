027ddb9e2130692742498e296fe2043274a0fbaa
longshao <dinosaurs1112@sina.com>
2017-02-24 20:58:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/myapplication1/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.android.myapplication1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "启动应用：热门电影", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button_2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "启动应用：股票雄鹰", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button_3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "启动应用：XYZ阅读器", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button_4);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "启动应用：最新闻", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button5 = (Button) findViewById(R.id.button_5);
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "启动应用：毕业设计", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
